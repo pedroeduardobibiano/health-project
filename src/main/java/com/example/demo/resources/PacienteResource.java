cab46f2a12d799569a3851792dab6903e9d71b36
Pedro Eduardo Bibiano Pereira <pedroedubibiano@gmail.com>
2025-01-06 20:14:13 -0300

    fix: "adding http post method "

--- src/main/java/com/example/demo/resources/PacienteResource.java
+++ src/main/java/com/example/demo/resources/PacienteResource.java
@@ -6,9 +6,7 @@ import com.example.demo.services.PacienteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,18 @@ public class PacienteResource {
     public ResponseEntity<List<PacienteDTO>> findAll() {
         List<PacienteDTO> dto = pacienteService.findAll();
         return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<PacienteDTO> findById(@PathVariable Long id) {
+        PacienteDTO dto = pacienteService.findById(id);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<PacienteDTO> save(@RequestBody PacienteDTO pacienteDTO) {
+        PacienteDTO dto = pacienteService.save(pacienteDTO);
+        return new ResponseEntity<>(dto, HttpStatus.CREATED);
     }
 
 
--- src/main/java/com/example/demo/services/PacienteService.java
+++ src/main/java/com/example/demo/services/PacienteService.java
@@ -5,8 +5,11 @@ import com.example.demo.entities.Paciente;
 import com.example.demo.repositories.PacienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,10 +17,34 @@ public class PacienteService {
 
     private final PacienteRepository pacienteRepository;
 
-
+    @Transactional(readOnly = true)
     public List<PacienteDTO> findAll() {
         List<Paciente> pacientes = pacienteRepository.findAll();
-        return pacientes.stream().map(x -> new PacienteDTO(x)).toList();
+        return pacientes.stream().map(PacienteDTO::new).toList();
     }
+
+    @Transactional(readOnly = true)
+    public PacienteDTO findById(Long id) {
+        Optional<Paciente> paciente = pacienteRepository.findById(id);
+        paciente.orElseThrow(() -> new NoSuchElementException("Id not found"));
+        return new PacienteDTO(paciente.get());
+    }
+
+    @Transactional
+    public PacienteDTO save(PacienteDTO pacienteDTO) {
+        Paciente save = getPaciente(pacienteDTO);
+        return new PacienteDTO(save);
+
+    }
+
+    private Paciente getPaciente(PacienteDTO pacienteDTO) {
+        Paciente paciente = new Paciente();
+        paciente.setCpf(pacienteDTO.getCpf());
+        paciente.setName(pacienteDTO.getName());
+        paciente.setDataNascimento(pacienteDTO.getDataNascimento());
+        paciente.setRg(pacienteDTO.getRg());
+        return pacienteRepository.save(paciente);
+    }
+
 
 }
